4ebfd79e05b585b0e94ec05359decc8b3d7732a8
neverland <chenjiahan.jait@bytedance.com>
2024-01-25 19:57:50 +0800

    docs: integrate rsfamily nav icon (#156)

--- document/theme/index.tsx
+++ document/theme/index.tsx
@@ -1,9 +1,9 @@
 import Theme from 'rspress/theme';
 import { HomeLayout } from './pages';
+import { RsfamilyNavIcon } from 'rsfamily-nav-icon';
+import 'rsfamily-nav-icon/dist/index.css';
 
-const Layout = () => (
-  <Theme.Layout />
-);
+const Layout = () => <Theme.Layout beforeNavTitle={<RsfamilyNavIcon />} />;
 
 // eslint-disable-next-line import/export
 export * from 'rspress/theme';
