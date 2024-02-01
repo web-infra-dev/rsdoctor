619549cbe30c4431d6626b4b8653595432455841
easy1090 <752355956@qq.com>
2024-02-01 21:41:30 +0800

    fix(rspack): builtin loader reporter
    fix(rspack): builtin loader reporter
    
    fix(rspack): builtin loader reporter

--- packages/rspack-plugin/src/builtinLoaderPlugin.ts
+++ packages/rspack-plugin/src/builtinLoaderPlugin.ts
@@ -1,5 +1,8 @@
-import { Compiler } from "@rspack/core";
-import path from "path";
+import { Compiler } from '@rspack/core';
+import { Utils } from '@rsdoctor/core/build-utils';
+import path from 'path';
+import { Plugin } from '@rsdoctor/types';
+import { RuleSetRules } from '@rspack/core';
 
 const BuiltinLoaderName = 'builtin:swc-loader';
 
@@ -7,22 +10,36 @@ export class BuiltinLoaderPlugin {
   apply(compiler: Compiler) {
     compiler.hooks.beforeRun.tap(
       {
-        name: 'CustomLoaderPlugin',
-      }, () => {
-        const rules = compiler.options.module.rules;
-        rules.forEach((rule) => {
-          if (rule && typeof rule === 'object' && Array.isArray(rule.use)) {
-            const _builtinLoaderRuleIndex = rule.use.findIndex((use) => typeof use === 'object' && use.loader.includes(BuiltinLoaderName));
-            if (_builtinLoaderRuleIndex !== -1) {
-              const _builtinLoaderRule = rule.use[_builtinLoaderRuleIndex];
-              const options = typeof _builtinLoaderRule === 'string' ? { type: 'start' } : { ..._builtinLoaderRule, type: 'start' };
-              rule.use.splice(_builtinLoaderRuleIndex, 0, { loader: path.join(__dirname, './probeLoader.js'), options: { ...options, type: 'end' } });
-              rule.use.splice(_builtinLoaderRuleIndex + 2, 0, { loader: path.join(__dirname, './probeLoader.js'), options });
-            }
+        name: 'BuiltinLoaderPlugin',
+      },
+      () => {
+        const rules = compiler.options.module.rules as Plugin.RuleSetRule[];
+        const appendRule = (rule: Plugin.RuleSetRule, index: number) => {
+          if ('use' in rule && Array.isArray(rule.use)) {
+            const _builtinRule = rule.use[index] as Plugin.RuleSetRule;
+            const _options =
+              typeof _builtinRule.options === 'string'
+                ? {}
+                : { ..._builtinRule };
+            rule.use.splice(index, 0, {
+              loader: path.join(__dirname, './probeLoader.js'),
+              options: { ..._options, type: 'end' },
+            });
+            rule.use.splice(index + 2, 0, {
+              loader: path.join(__dirname, './probeLoader.js'),
+              options: { ..._options, type: 'start' },
+            });
           }
-        });
+          return rule;
+        };
 
-        compiler.options.module.rules = rules;
-      });
+        compiler.options.module.rules =
+          Utils.changeBuiltinLoader<Plugin.RuleSetRule>(
+            rules,
+            BuiltinLoaderName,
+            appendRule,
+          ) as RuleSetRules;
+      },
+    );
   }
 }
