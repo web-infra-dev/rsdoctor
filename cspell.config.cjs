2e23a4c8a96cea3a6b232c2f893a69b0770369a4
neverland <chenjiahan.jait@bytedance.com>
2024-04-02 15:41:10 +0800

    chore(workflow): integrate cspell-ban-words

---
+++ cspell.config.cjs
@@ -0,0 +1,30 @@
+const { banWords } = require('cspell-ban-words');
+
+module.exports = {
+  version: '0.2',
+  language: 'en',
+  files: ['**/*.{ts,tsx,js,jsx,md,mdx}'],
+  enableFiletypes: ['mdx'],
+  ignoreRegExpList: [
+    // ignore markdown anchors such as [fooBar](#foobar)
+    '#.*?\\)',
+  ],
+  ignorePaths: [
+    'dist',
+    'dist-*',
+    'compiled',
+    'coverage',
+    'doc_build',
+    'node_modules',
+    'pnpm-lock.yaml',
+  ],
+  flagWords: banWords,
+  dictionaries: ['dictionary'],
+  dictionaryDefinitions: [
+    {
+      name: 'dictionary',
+      path: './scripts/dictionary.txt',
+      addWords: true,
+    },
+  ],
+};
