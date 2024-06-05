67b16ed676da249dad7bffe416a3fd638c58655d
easy1090 <752355956@qq.com>
2024-06-05 16:57:31 +0800

    fix(rspack): the reportCodeType is not work problem

--- examples/rsbuild-minimal/rsbuild.config.ts
+++ examples/rsbuild-minimal/rsbuild.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
         {
           disableClientServer: !process.env.ENABLE_CLIENT_SERVER,
           features: ['bundle', 'plugins', 'loader', 'resolver'],
+          reportCodeType: {
+            noAssetsAndModuleSource: true,
+          },
         },
       ]);
     },
