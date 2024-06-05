361bc226ee73e62f11dae9b72627cda6b52e7a8e
nanianlisao <597219320@qq.com>
2024-06-05 10:33:41 +0800

    chore: Get the version of pnpm with getpnpmInfo

--- packages/utils/src/build/envinfo.ts
+++ packages/utils/src/build/envinfo.ts
@@ -1,40 +1,36 @@
 import { helpers, run } from 'envinfo';
 import { exec } from 'child_process';
 
-export const getCPUInfo = () =>
-  helpers.getCPUInfo().then((res) => res[1]);
+export const getCPUInfo = () => helpers.getCPUInfo().then((res) => res[1]);
 
-export const getOSInfo = () =>
-  helpers.getOSInfo().then((res) => res[1]);
+export const getOSInfo = () => helpers.getOSInfo().then((res) => res[1]);
 
 export const getMemoryInfo = () =>
   helpers.getMemoryInfo().then((res) => res[1]);
 
-export const getNodeVersion = () =>
-  helpers.getNodeInfo().then((res) => res[1]);
+export const getNodeVersion = () => helpers.getNodeInfo().then((res) => res[1]);
 
-export const getYarnVersion = () =>
-  helpers.getYarnInfo().then((res) => res[1]);
+export const getYarnVersion = () => helpers.getYarnInfo().then((res) => res[1]);
 
-export const getNpmVersion = () =>
-  helpers.getnpmInfo().then((res) => res[1]);
+export const getNpmVersion = () => helpers.getnpmInfo().then((res) => res[1]);
+
+export const getPnpmVersion = () => helpers.getpnpmInfo().then((res) => res[1]);
 
 export function getNpmPackageVersion(pkg: string): Promise<string>;
 export function getNpmPackageVersion(pkgs: string[]): Promise<string[]>;
 export function getNpmPackageVersion(pkg: unknown): Promise<unknown> {
   const isArray = Array.isArray(pkg);
   return run(
-      {
-        npmPackages: isArray ? pkg : [pkg],
-      },
-      { json: true, showNotFound: true },
-    )
-    .then((res) => {
-      const { npmPackages = {} } = JSON.parse(res) || {};
-      return isArray
-        ? pkg.map((e) => npmPackages[e] || 'Not Found')
-        : npmPackages[pkg as string];
-    });
+    {
+      npmPackages: isArray ? pkg : [pkg],
+    },
+    { json: true, showNotFound: true },
+  ).then((res) => {
+    const { npmPackages = {} } = JSON.parse(res) || {};
+    return isArray
+      ? pkg.map((e) => npmPackages[e] || 'Not Found')
+      : npmPackages[pkg as string];
+  });
 }
 
 export function getGlobalNpmPackageVersion(pkg: string): Promise<string>;
@@ -42,17 +38,16 @@ export function getGlobalNpmPackageVersion(pkgs: string[]): Promise<string[]>;
 export function getGlobalNpmPackageVersion(pkg: unknown): Promise<unknown> {
   const isArray = Array.isArray(pkg);
   return run(
-      {
-        npmGlobalPackages: isArray ? pkg : [pkg],
-      },
-      { json: true, showNotFound: true },
-    )
-    .then((res) => {
-      const { npmGlobalPackages = {} } = JSON.parse(res) || {};
-      return isArray
-        ? pkg.map((e) => npmGlobalPackages[e] || 'Not Found')
-        : npmGlobalPackages[pkg as string];
-    });
+    {
+      npmGlobalPackages: isArray ? pkg : [pkg],
+    },
+    { json: true, showNotFound: true },
+  ).then((res) => {
+    const { npmGlobalPackages = {} } = JSON.parse(res) || {};
+    return isArray
+      ? pkg.map((e) => npmGlobalPackages[e] || 'Not Found')
+      : npmGlobalPackages[pkg as string];
+  });
 }
 
 export function getGitBranch() {
