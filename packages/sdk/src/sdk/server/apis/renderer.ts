586982c4f8488f8290b61769b68e7a892a203e2f
yifancong <easy_cong@126.com>
2024-07-12 19:05:43 +0800

    fix(server): the client page get error on '/' path (#421)

--- packages/sdk/src/sdk/server/apis/renderer.ts
+++ packages/sdk/src/sdk/server/apis/renderer.ts
@@ -1,13 +1,9 @@
 import { SDK } from '@rsdoctor/types';
-import serve from 'serve-static';
-import path from 'path';
 import { File } from '@rsdoctor/utils/build';
 import { BaseAPI } from './base';
 import { Router } from '../router';
 
 export class RendererAPI extends BaseAPI {
-  private isClientServed = false;
-
   /** sdk manifest api */
   @Router.get(SDK.ServerAPI.API.EntryHtml)
   public async entryHtml(): Promise<
@@ -20,11 +16,7 @@ export class RendererAPI extends BaseAPI {
     const clientHtmlPath = server.innerClientPath
       ? server.innerClientPath
       : require.resolve('@rsdoctor/client');
-    if (!this.isClientServed) {
-      this.isClientServed = true;
-      const clientDistPath = path.resolve(clientHtmlPath, '..');
-      server.app.use(serve(clientDistPath));
-    }
+
     const clientHtml = await File.fse.readFile(clientHtmlPath, 'utf-8');
 
     res.setHeader('Content-Type', 'text/html');
