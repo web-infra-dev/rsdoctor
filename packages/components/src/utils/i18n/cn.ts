0bcce00b51ad2aeac720b0a74ed93d5af169416f
easy1090 <congzhe@bytedance.com>
2024-09-30 14:40:51 +0800

    chore(docs): add bundle-diff documents

--- packages/components/src/utils/i18n/cn.ts
+++ packages/components/src/utils/i18n/cn.ts
@@ -17,20 +17,25 @@ const cn: typeof en = {
   'Project Overall': '项目信息概览',
   'Compile Overall': '编译数据概览',
   'Bundle Overall': '产物数据概览',
-  'Expand Omitted': ' reasons 树会默认折叠中间的第三方引入的模块，点击这个图标可以查看完整树图',
+  'Expand Omitted':
+    ' reasons 树会默认折叠中间的第三方引入的模块，点击这个图标可以查看完整树图',
   'Concatenated Tag':
     '串联模块，hover 上去可以显示聚合成的主模块名称。注：聚合模块 bundled size 为零，有可能是因为被聚合到了主模块中。',
 
-  'the file content not changed after transpiled by this loader': '该文件内容在 loader 处理前后没有发生变化',
+  'the file content not changed after transpiled by this loader':
+    '该文件内容在 loader 处理前后没有发生变化',
 
   /** Bundle Size */
   'Output Assets List': '产物文件列表',
-  'filter the output assets which size is greater than the input value': '筛选大小大于输入值的产物文件',
-  'filter the modules which size is greater than the input value': '筛选大小大于输入值的模块',
+  'filter the output assets which size is greater than the input value':
+    '筛选大小大于输入值的产物文件',
+  'filter the modules which size is greater than the input value':
+    '筛选大小大于输入值的模块',
   'After Compile': '编译后',
   'After Bundle': '打包后',
   CodeModeExplain: 'Lite 模式下或纯 stats.json 上传的情况下只有 source code.',
-  DuplicatePakCodeExplain: '纯 stats.json 输入的情况下没有代码，可使用 web-doctor 插件参与构建会有对应代码。',
+  DuplicatePakCodeExplain:
+    '纯 stats.json 输入的情况下没有代码，可使用 Rsdoctor 插件参与构建会有对应代码。',
 };
 
 export default cn;
