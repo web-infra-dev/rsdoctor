d5d0fa5d5667c85e3d6b6cad78478b44ddbc3cd5
LingyuCoder <lingyucoder@gmail.com>
2025-01-13 14:20:47 +0800

    refactor: interface oriented programming

--- packages/graph/src/graph/chunk-graph/graph.ts
+++ packages/graph/src/graph/chunk-graph/graph.ts
@@ -1,49 +1,65 @@
 import { SDK } from '@rsdoctor/types';
 
 export class ChunkGraph implements SDK.ChunkGraphInstance {
-  private _assetMap: Map<string, SDK.AssetInstance> = new Map();
-  private _chunkMap: Map<string, SDK.ChunkInstance> = new Map();
-  private _entrypointMap: Map<string, SDK.EntryPointInstance> = new Map();
+  private _assets: SDK.AssetInstance[] = [];
+  private _chunks: SDK.ChunkInstance[] = [];
+  private _entrypoints: SDK.EntryPointInstance[] = [];
+  private _assetMap: Record<string, SDK.AssetInstance> = {};
+  private _chunkMap: Record<string, SDK.ChunkInstance> = {};
+  private _entrypointMap: Record<string, SDK.EntryPointInstance> = {};
 
   getAssets(): SDK.AssetInstance[] {
-    return Array.from(this._assetMap.values());
+    return this._assets.slice();
   }
 
   getChunks(): SDK.ChunkInstance[] {
-    return Array.from(this._chunkMap.values());
+    return this._chunks.slice();
   }
 
   addAsset(...assets: SDK.AssetInstance[]): void {
     assets.forEach((asset) => {
-      this._assetMap.set(asset.path, asset);
+      if (!this._assets.includes(asset)) {
+        this._assets.push(asset);
+        this._assetMap[asset.path] = asset;
+      }
     });
   }
 
   addChunk(...chunks: SDK.ChunkInstance[]): void {
     chunks.forEach((chunk) => {
-      this._chunkMap.set(chunk.id, chunk);
+      if (!this._chunks.includes(chunk)) {
+        this._chunks.push(chunk);
+        this._chunkMap[chunk.id] = chunk;
+      }
     });
   }
 
   getChunkById(id: string): SDK.ChunkInstance | undefined {
-    return this._chunkMap.get(id);
+    return this._chunkMap[id];
+  }
+
+  getChunkByModule(module: SDK.ModuleInstance): SDK.ChunkInstance | undefined {
+    return this._chunks.find((item) => item.hasModule(module));
   }
 
   getAssetByPath(path: string): SDK.AssetInstance | undefined {
-    return this._assetMap.get(path);
+    return this._assetMap[path];
   }
 
   getEntryPoints(): SDK.EntryPointInstance[] {
-    return Array.from(this._entrypointMap.values());
+    return this._entrypoints.slice();
   }
 
   getEntryPointByName(name: string): SDK.EntryPointInstance | undefined {
-    return this._entrypointMap.get(name);
+    return this._entrypointMap[name];
   }
 
   addEntryPoint(...entrypoints: SDK.EntryPointInstance[]): void {
     entrypoints.forEach((entrypoint) => {
-      this._entrypointMap.set(entrypoint.name, entrypoint);
+      if (!this._entrypoints.includes(entrypoint)) {
+        this._entrypoints.push(entrypoint);
+        this._entrypointMap[entrypoint.name] = entrypoint;
+      }
     });
   }
 
