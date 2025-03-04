eaa768ab79a9a3bf7991b12a38b56d7f225179a9
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2025-03-04 14:02:31 -0500

    fix(ui): add spacing before size value

--- packages/components/src/pages/BundleSize/components/asset.tsx
+++ packages/components/src/pages/BundleSize/components/asset.tsx
@@ -409,23 +409,23 @@ export const AssetDetail: React.FC<{
                   title={
                     <Space direction="vertical">
                       <Tag color={'orange'}>
-                        {'Bundled Size:' + formatSize(parsedSize)}
+                        {`Bundled Size: ${formatSize(parsedSize)}`}
                       </Tag>
                       <Tag color={'volcano'}>
-                        {'Source Size:' + formatSize(sourceSize)}
+                        {`Source Size: ${formatSize(sourceSize)}`}
                       </Tag>
                     </Space>
                   }
                   color={'white'}
                 >
                   <Tag color={'purple'} style={tagStyle}>
-                    {'Bundled: ' + formatSize(parsedSize)}
+                    {`Bundled: ${formatSize(parsedSize)}`}
                   </Tag>
                 </Tooltip>
               ) : sourceSize !== 0 ? (
                 // fallback to display tag for source size
                 <Tag color={'geekblue'}>
-                  {'Source Size:' + formatSize(sourceSize)}
+                  {`Source: ${formatSize(sourceSize)}`}
                 </Tag>
               ) : null}
               {isConcatenation ? (
