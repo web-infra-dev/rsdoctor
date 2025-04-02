729d169ec9fc4a389d964861714723a535b79d84
yifancong <easy_cong@126.com>
2025-04-02 10:31:11 +0800

    Add Ai test fixtures. (#893)

--- packages/utils/src/logger.ts
+++ packages/utils/src/logger.ts
@@ -15,3 +15,32 @@ export function debug(getMsg: () => string, prefix = '') {
 }
 
 export { chalk, logger };
+
+logger.override({
+  log: (message) => {
+    console.log(`[Rsdoctor log] ${message}`);
+  },
+  info: (message) => {
+    console.log(`[Rsdoctor info] ${message}`);
+  },
+  warn: (message) => {
+    console.warn(`[Rsdoctor warn] ${message}`);
+  },
+  start: (message) => {
+    console.log(`[Rsdoctor start] ${message}`);
+  },
+  ready: (message) => {
+    console.log(`[Rsdoctor ready] ${message}`);
+  },
+  error: (message) => {
+    console.error(`[Rsdoctor error] ${message}`);
+  },
+  success: (message) => {
+    console.error(`[Rsdoctor success] ${message}`);
+  },
+  debug: (message) => {
+    if (process.env.DEBUG) {
+      console.log(`[Rsdoctor debug] ${message}`);
+    }
+  },
+});
