55bf869b6f319123557d58b1448975e042372a0e
yifancong <easy_cong@126.com>
2025-04-02 10:31:11 +0800

    Add Ai test fixtures. (#893)

--- packages/ai/src/mcp/socket.ts
+++ packages/ai/src/mcp/socket.ts
@@ -1,14 +1,15 @@
 import { Socket, io } from 'socket.io-client';
-
+import { logger } from '@rsdoctor/utils/logger';
 const map: Record<string, Socket> = {};
 
-export const createSocket = (url: string) => {
-  console.log(map[url]);
+// 使用 logger.error 输出日志
+export const createSocket = (url: string): Socket => {
+  logger.error(map[url]);
   if (map[url]) return map[url];
   const socket = io(url, {});
-  console.log('socket created', url);
+  logger.error('socket created', url);
   socket.on('connect', () => {
-    console.log(`Socket Connect ${url}`);
+    logger.error(`Socket Connect ${url}`);
   });
   map[url] = socket;
   return socket;
@@ -16,14 +17,13 @@ export const createSocket = (url: string) => {
 
 export const getWsUrl = async () => {
   // TODO: get port from rsdoctor server
-  return 'ws://localhost:3021';
+  return 'ws://localhost:4796';
 };
 
 export const sendRequest = async (api: string, params = {}) => {
   const url = await getWsUrl();
-  console.log('url', url);
   const socket = createSocket(url);
-  console.log('socket', socket);
+  logger.error('[mcp]socket client is started');
   const res = await new Promise((resolve) => {
     socket.emit(api, params, (res: any) => {
       resolve(res.res);
