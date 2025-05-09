a58824bf49cb11fa344b19bf415ccf276b8e9c0f
yifancong <easy_cong@126.com>
2025-05-09 18:07:50 +0800

    fix: export the DiffContainer (#1078)

--- packages/components/src/pages/Resources/BundleDiff/index.tsx
+++ packages/components/src/pages/Resources/BundleDiff/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { DiffContainer } from './DiffContainer';
 
 export * from './constants';
+export * from './DiffContainer';
 
 export const Page: React.FC = () => {
   return <DiffContainer manifests={[]} />;
