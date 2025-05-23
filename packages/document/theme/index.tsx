e6b990b8c0141a827f92b19b401f7eb313077a84
sunyiteng <sunyiteng@bytedance.com>
2025-05-23 15:39:39 +0800

    docs(rspress): use @rspress/plugin-algolia for search and upgrade rspress version

--- packages/document/theme/index.tsx
+++ packages/document/theme/index.tsx
@@ -1,10 +1,31 @@
 import { Layout as BaseLayout } from 'rspress/theme';
 import { NavIcon } from '@rstack-dev/doc-ui/nav-icon';
 import { HomeLayout } from './pages';
+import {
+  Search as PluginAlgoliaSearch,
+  ZH_LOCALES,
+} from '@rspress/plugin-algolia/runtime';
+import { useLang } from 'rspress/runtime';
 
 const Layout = () => <BaseLayout beforeNavTitle={<NavIcon />} />;
 
-export { Layout, HomeLayout };
+const Search = () => {
+  const lang = useLang();
+  return (
+    <PluginAlgoliaSearch
+      docSearchProps={{
+        appId: 'TQOGCXPBUD', // cspell:disable-line
+        apiKey: '8c30f9d1f12e786a132af15ea30cf997', // cspell:disable-line
+        indexName: 'rspack',
+        searchParameters: {
+          facetFilters: [`lang:${lang}`],
+        },
+      }}
+      locales={ZH_LOCALES}
+    />
+  );
+};
 
+export { Layout, HomeLayout, Search };
 // eslint-disable-next-line import/export
 export * from 'rspress/theme';
