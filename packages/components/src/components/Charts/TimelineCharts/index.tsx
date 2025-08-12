9d86b4986e6306dddf523ad728aace2af22e484b
yifancong <easy_cong@126.com>
2025-08-12 14:07:56 +0800

    fix: change echarts-for-react import type (#1244)

--- packages/components/src/components/Charts/TimelineCharts/index.tsx
+++ packages/components/src/components/Charts/TimelineCharts/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, memo } from 'react';
-import ReactEChartsCore from 'echarts-for-react/lib/core';
+import ReactEChartsCore from 'echarts-for-react/esm/core';
 import * as echarts from 'echarts/core';
 import { CustomChart } from 'echarts/charts';
 import {
