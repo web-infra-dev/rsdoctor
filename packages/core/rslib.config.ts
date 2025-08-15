b2152fc6012cc0d83cd5474ed574a564e9c5c958
easy1090 <congzhe@bytedance.com>
2025-08-15 14:14:25 +0800

    chore: update to dual packages

--- packages/core/rslib.config.ts
+++ packages/core/rslib.config.ts
@@ -1,5 +1,6 @@
 import { rsbuild, rspack } from '@rslib/core';
 import { dualPackageBundleless } from '../../scripts/rslib.base.config';
+import path from 'path';
 
 export default rsbuild.mergeRsbuildConfig(dualPackageBundleless, {
   tools: {
@@ -7,7 +8,10 @@ export default rsbuild.mergeRsbuildConfig(dualPackageBundleless, {
       plugins: [
         new rspack.BannerPlugin({
           banner: (args) => {
-            if (args.filename === 'inner-plugins/loaders/proxy.js') {
+            const normalizedFilename = path.posix.normalize(
+              args.filename.replace(/\\/g, '/'),
+            );
+            if (normalizedFilename === 'inner-plugins/loaders/proxy.js') {
               return 'module.exports = loaderModule; // This is a proxy loader, do not remove this line';
             }
             return '';
