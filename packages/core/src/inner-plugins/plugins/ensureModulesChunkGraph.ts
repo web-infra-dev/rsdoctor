6b4bf20e0d71fac01e76c83b0e05e176efae3177
easy1090 <congzhe@bytedance.com>
2025-11-13 11:25:44 +0800

    fix: module graph lack some modules in rspeedy

--- packages/core/src/inner-plugins/plugins/ensureModulesChunkGraph.ts
+++ packages/core/src/inner-plugins/plugins/ensureModulesChunkGraph.ts
@@ -124,17 +124,7 @@ async function doneHandler(
     let cached: Plugin.StatsCompilation | null = null;
     return () => {
       if (cached) return cached as Plugin.StatsCompilation;
-      cached = stats.toJson({
-        all: false,
-        chunks: true,
-        modules: true,
-        chunkModules: true,
-        assets: true,
-        ids: true,
-        hash: true,
-        errors: true,
-        warnings: true,
-      });
+      cached = stats.toJson();
       return cached;
     };
   })();
