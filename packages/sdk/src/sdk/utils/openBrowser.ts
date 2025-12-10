8b9d7a92413cd027ea440ea0621112fe4d06a082
congzhe <congzhe@bytedance.com>
2025-12-10 17:20:03 +0800

    fix: open browser problem

--- packages/sdk/src/sdk/utils/openBrowser.ts
+++ packages/sdk/src/sdk/utils/openBrowser.ts
@@ -65,18 +65,12 @@ export async function openBrowser(
         return true;
       }
       logger.debug('Failed to find the target browser.');
-    } catch (err) {
-      logger.debug('Failed to open Rsdoctor URL with apple script.');
-      logger.debug(err);
-    }
-    try {
       const { default: open } = await import('open');
       await open(url);
       return true;
     } catch (err) {
-      logger.error('Failed to open Rsdoctor URL.');
-      logger.error(err);
-      return false;
+      logger.debug('Failed to open Rsdoctor URL with apple script.');
+      logger.debug(err);
     }
   } else {
     // Fallback to open
