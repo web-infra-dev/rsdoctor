559fe14ece0497ff6ee88249a882773d0459b46e
Soon <sooniter@gmail.com>
2025-12-10 19:11:37 +0800

    docs: fix no features in homepage (#1438)

--- packages/document/theme/components/Features.tsx
+++ packages/document/theme/components/Features.tsx
@@ -8,44 +8,42 @@ import './Features.module.scss';
 
 export function Features() {
   const t = useI18n<typeof import('i18n')>();
-  const features = {
-    features: [
-      {
-        title: t('frameworkAgnostic'),
-        details: t('frameworkAgnosticDesc'),
-        icon: '🛠️',
-      },
-      {
-        title: t('buildTime'),
-        details: t('buildTimeDesc'),
-        icon: '🚀',
-      },
-      {
-        title: t('buildActions'),
-        details: t('buildActionsDesc'),
-        icon: '🦄',
-      },
-      {
-        title: t('bundleAnalysis'),
-        details: t('bundleAnalysisDesc'),
-        icon: '🎯',
-      },
-      {
-        title: t('bundleDiff'),
-        details: t('bundleDiffDesc'),
-        icon: '🎨',
-      },
-      {
-        title: t('buildScan'),
-        details: t('buildScanDesc'),
-        icon: '🔍',
-      },
-    ],
-  };
+  const features = [
+    {
+      title: t('frameworkAgnostic'),
+      details: t('frameworkAgnosticDesc'),
+      icon: '🛠️',
+    },
+    {
+      title: t('buildTime'),
+      details: t('buildTimeDesc'),
+      icon: '🚀',
+    },
+    {
+      title: t('buildActions'),
+      details: t('buildActionsDesc'),
+      icon: '🦄',
+    },
+    {
+      title: t('bundleAnalysis'),
+      details: t('bundleAnalysisDesc'),
+      icon: '🎯',
+    },
+    {
+      title: t('bundleDiff'),
+      details: t('bundleDiffDesc'),
+      icon: '🎨',
+    },
+    {
+      title: t('buildScan'),
+      details: t('buildScanDesc'),
+      icon: '🔍',
+    },
+  ];
   return (
     <section className={containerStyle}>
       <div className={innerContainerStyle}>
-        <HomeFeature frontmatter={features} routePath="/" />
+        <HomeFeature features={features} />
       </div>
     </section>
   );
