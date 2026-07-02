3f59b1b9b4b632676dc15d12469a1fbdefa8aa6f
yifancong <congzhe@bytedance.com>
2026-07-02 18:57:59 +0800

    fix(core): export rspack plugin from root entry

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,5 @@
 export * from './build-utils';
 export * as InnerPlugins from './inner-plugins';
+export { RsdoctorRspackPlugin } from './rspack-plugin/plugin';
+export { RsdoctorRspackMultiplePlugin } from './rspack-plugin/multiple';
 export * from './types';
