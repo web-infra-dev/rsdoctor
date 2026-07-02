55a7fa968976ecd19573b1a73e23537f77416169
yifancong <congzhe@bytedance.com>
2026-07-02 16:08:45 +0800

    fix(shared): consolidate loader type imports

--- packages/shared/src/common/loader.ts
+++ packages/shared/src/common/loader.ts
@@ -1,6 +1,5 @@
-import { SDK } from '../types';
+import { Plugin, SDK } from '../types';
 import { mergeIntervals } from './algorithm';
-import { Plugin } from '../types';
 
 export function findLoaderTotalTiming(
   loaders: Pick<SDK.LoaderTransformData, 'startAt' | 'endAt'>[],
