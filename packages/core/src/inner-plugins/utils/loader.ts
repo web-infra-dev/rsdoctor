c342a3c0f7659be87239531aabb577395ed8c195
easy1090 <congzhe@bytedance.com>
2026-07-24 16:25:01 +0800

    refactor: support esm only

--- packages/core/src/inner-plugins/utils/loader.ts
+++ packages/core/src/inner-plugins/utils/loader.ts
@@ -124,7 +124,11 @@ export function interceptLoader<T extends Plugin.BuildRuleSetRule>(
     };
     // In the childCompiler of mini-css-extract-plugin, the options[Loader.LoaderInternalPropertyName] of proxy-loader is set to proxy-loader, ultimately causing errors in the compilation of less files.
     opts[Loader.LoaderInternalPropertyName] =
-      rule.loader && 'options' in rule && typeof rule.options === 'object'
+      rule.loader &&
+      // compatible with proxy.cjs and proxy.js
+      /proxy\.(js|cjs)/.test(rule.loader) &&
+      'options' in rule &&
+      typeof rule.options === 'object'
         ? rule.options[Loader.LoaderInternalPropertyName]
         : {
             ...options,
