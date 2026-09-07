53341fdbc72d92277aa70de32bd9e129936df198
Elecmonkey <elecmonkey@hotmail.com>
2026-09-07 19:17:55 +0800

    enable type check

--- examples/rspress-minimal/rspress.config.ts
+++ examples/rspress-minimal/rspress.config.ts
@@ -1,5 +1,5 @@
 import * as path from 'node:path';
-import { defineConfig } from 'rspress/config';
+import { defineConfig } from '@rspress/core';
 import { RsdoctorRspackPlugin } from '@rsdoctor/core';
 
 export default defineConfig({
