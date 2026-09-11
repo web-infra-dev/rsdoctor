d1652aef704029d75f8738699c52cb8451b246d7
neverland <chenjiahan.jait@bytedance.com>
2026-09-11 10:30:03 +0800

    chore: remove unused files

--- packages/core/src/common/file.ts
+++
@@ -1,7 +0,0 @@
-export function isStyleExt(path: string) {
-  return /\.(c|le|sa|sc)ss(\?.*)?$/.test(path);
-}
-
-export function isJsExt(path: string) {
-  return /\.(js|ts|jsx|tsx)(\?.*)?$/.test(path);
-}
